a265de2c7e13a4367b67c5bbbe94ebf3beca1eb6
Gerardox1029 <60759191@continental.edu.pe>
2026-06-23 20:49:29 -0500

    + POPCATUSDT

--- src/config.js
+++ src/config.js
@@ -4,13 +4,13 @@ const path = require('path');
 const SYMBOLS = [
     'BTCUSDT', 'ETHUSDT', 'SOLUSDT', 'BNBUSDT', // Large Caps
     'DOGEUSDT', 'AVAXUSDT', 'ADAUSDT', 'BCHUSDT','AAVEUSDT',// Mid Caps
-    'RENDERUSDT', 'NEARUSDT', 'WLDUSDT', 'SUIUSDT' // Small Caps
+    'RENDERUSDT', 'NEARUSDT', 'WLDUSDT', 'SUIUSDT','POPCATUSDT' // Small Caps
 ];
 
 const CATEGORIES = {
     'Large Caps': ['BTCUSDT', 'ETHUSDT', 'SOLUSDT', 'BNBUSDT'],
     'Mid Caps': ['DOGEUSDT', 'AVAXUSDT', 'ADAUSDT', 'BCHUSDT','AAVEUSDT'],
-    'Small Caps': ['RENDERUSDT', 'NEARUSDT', 'WLDUSDT', 'SUIUSDT']
+    'Small Caps': ['RENDERUSDT', 'NEARUSDT', 'WLDUSDT', 'SUIUSDT','POPCATUSDT']
 };
 
 const INTERVALS = ['2h'];
